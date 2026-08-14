c0e497e51144b3b5ec27b89f2770eaf3fd6d2cda
Hiran Lakshika <hiranlakshika@gmail.com>
2026-08-14 08:17:13 +0530

    Made multiple answer 4

--- src/main/kotlin/com/quiz/famousquotequizbackend/quiz/QuizService.kt
+++ src/main/kotlin/com/quiz/famousquotequizbackend/quiz/QuizService.kt
@@ -13,7 +13,7 @@ import java.time.Instant
 @ConfigurationProperties("quiz")
 data class QuizProperties(
     val questionsPerSession: Int = 10,
-    val multipleChoiceOptions: Int = 3,
+    val multipleChoiceOptions: Int = 4,
 )
 
 @Service
